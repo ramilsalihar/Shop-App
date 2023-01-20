c48893d057013f1ae0d20ec178119b71e2935aed
ramil.salihar <ramil.salihar_2025@ucentralasia.org>
2023-01-21 00:52:12 +0600

    <design> redesigning drawer

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 final theme = ThemeData(
-  primarySwatch: Colors.purple,
+  primaryColor: Colors.black,
+  cardColor: Colors.white,
   colorScheme: ColorScheme.fromSwatch().copyWith(
     secondary: Colors.deepOrange,
   ),
   fontFamily: 'Lato',
+  textTheme: const TextTheme(
+    headline1: TextStyle(fontSize: 16, color: Colors.white)
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.black,
+    )
+  ),
+  appBarTheme: const AppBarTheme(
+    color: Colors.black,
+  )
+
 );
